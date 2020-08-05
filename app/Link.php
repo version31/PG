99ac3f03c27a6e92fe5c02a43a4294bcec3c72b2
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-08-06 00:07:46 +0430

    add ide-helper

--- app/Link.php
+++ app/Link.php
@@ -4,6 +4,22 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Link
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $value
+ * @property string $type
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Link newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Link newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Link query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Link whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Link whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Link whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Link whereValue($value)
+ * @mixin \Eloquent
+ */
 class Link extends Model
 {
     //
--- app/Page.php
+++ app/Page.php
@@ -4,6 +4,38 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Page
+ *
+ * @property int $id
+ * @property int $author_id
+ * @property string $title
+ * @property string|null $excerpt
+ * @property string|null $body
+ * @property string|null $media_path
+ * @property string $slug
+ * @property string|null $meta_description
+ * @property string|null $meta_keywords
+ * @property string $status
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereAuthorId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereBody($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereExcerpt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereMediaPath($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereMetaDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereMetaKeywords($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereStatus($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Page whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Page extends Model
 {
     //

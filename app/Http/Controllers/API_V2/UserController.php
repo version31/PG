cabbc34b5a6fe180811100cc8f9a5725b57d65ab
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-08-08 01:10:26 +0430

    add profile

--- app/Http/Controllers/API_V2/UserController.php
+++ app/Http/Controllers/API_V2/UserController.php
@@ -5,11 +5,13 @@ namespace App\Http\Controllers\API_V2;
 use App\Category;
 use App\Facades\ResultData;
 use App\Http\Controllers\Controller;
+use App\Http\Resources\BasicResource;
 use App\Http\Resources\ShowStatusResource;
 use App\Likeable;
 use App\Link;
 use App\Direct;
 use App\Product;
+use App\Sh4\sh4Action;
 use App\Sh4\sh4Auth;
 use App\Sh4\sh4Tools;
 use App\Storyable;
@@ -27,7 +29,7 @@ use Validator;
 
 class UserController extends Controller
 {
-    use sh4Auth, sh4Tools;
+    use sh4Auth, sh4Tools, sh4Action;
 
     public function showStatus()
     {
@@ -48,5 +50,30 @@ class UserController extends Controller
         return new ShowStatusResource($query);
 
     }
+
+
+    public function currentUser()
+    {
+
+        $query = User::where('id', Auth::id())->with(['city', 'role', 'favoritePosts', 'favoriteProducts', 'links', 'products' => function ($query) {
+            $query->orderBy('id', 'Desc')->with('addables');
+        }])
+            ->first();
+
+
+        return new BasicResource($query);
+
+    }
+
+
+    public function catalogs($id)
+    {
+        if ($id == "current")
+            $id = Auth::id();
+
+        $query = User::select('id','first_name','last_name')->where('id', $id)->with('catalogs')->first();
+        return new BasicResource($query);
+    }
+
 
 }

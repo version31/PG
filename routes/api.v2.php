a4c2518433598d6b7d5bb0b59ec41673acff7c7c
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-08-08 01:10:35 +0430

    add routes to api.v2

--- routes/api.v2.php
+++ routes/api.v2.php
@@ -33,14 +33,6 @@ Route::get('providers', 'ProviderController@index'); #7
 
 
 
-/*
-|--------------------------------------------------------------------------
-| Payment
-|--------------------------------------------------------------------------
-|
-*/
-Route::get('/payment-plans/{planId}', 'PaymentPlanController@payment');
-
 
 
 /*
@@ -52,8 +44,12 @@ Route::get('/payment-plans/{planId}', 'PaymentPlanController@payment');
 Route::post('/wallet/transfer', 'WalletController@transfer');
 Route::post('/wallet/money-request', 'WalletController@moneyRequest');
 Route::get('/wallet/transactions', 'WalletController@transactions');
+Route::get('/wallet/buy-plans/{planId}', 'PaymentPlanController@payment');
 
 
+//payment
+Route::post('/wallet/increase/callback', 'PaymentController@callback');
+Route::post('/wallet/increase', 'PaymentController@do');
 
 /*
 |--------------------------------------------------------------------------
@@ -75,5 +71,37 @@ Route::get('/variables/{key}', 'VariableController@show');
 
 
 
-Route::post('/wallet/increase/callback', 'PaymentController@callback');
-Route::post('/wallet/increase', 'PaymentController@do');
+
+
+/*
+|--------------------------------------------------------------------------
+| Version 01
+|--------------------------------------------------------------------------
+|
+*/
+
+Route::get('activities/main', 'ActivityController@showMain');
+Route::get('/users/current', 'UserController@currentUser');
+
+
+/*
+|--------------------------------------------------------------------------
+| Version 01
+|--------------------------------------------------------------------------
+|
+*/
+
+
+Route::put('products/action', 'ProductController@act');
+Route::put('posts/action', 'PostController@act');
+Route::put('categories/action', 'CategoryController@act');
+Route::put('users/action', 'UserController@act');
+
+
+
+Route::post('catalogs', 'CatalogController@store');
+Route::delete('catalogs/{id}', 'CatalogController@destroy');
+Route::put('catalogs/{id}', 'CatalogController@update');
+
+
+Route::get('users/{id}/catalogs', 'UserController@catalogs');

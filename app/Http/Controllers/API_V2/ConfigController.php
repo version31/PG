e145718e4f955b60f4588127bb9d65049afc807f
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-08-28 12:37:02 +0430

    add ip to config

--- app/Http/Controllers/API_V2/ConfigController.php
+++ app/Http/Controllers/API_V2/ConfigController.php
@@ -17,6 +17,7 @@ class ConfigController extends Controller
         $data['minimum_version'] = $this->minimumVersionAPI;
         $data['current_version'] = $this->getCurrentVersion();
         $data['current_user'] = Auth::user() ? Auth::user()->only(['id', 'name', 'mobile', 'status', 'shop_expired_at', 'limit_insert_product', 'role']) : null;
+        $data['ip'] = \Request::ip();
 
         return new BasicResource($data);
     }

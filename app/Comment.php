653b05291bf11e3fcdc9ce4cbd0ae86c4e4beb59
Ali Sharifi Neyestani <sh4rifi@gmail.com>
2020-12-24 16:55:19 +0330

    Added parentId to comments

--- app/Comment.php
+++ app/Comment.php
@@ -7,8 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     //
+
+    protected $fillable = [
+        'title',
+        'score',
+        'body',
+        'positive_items',
+        'negative_items',
+        'user_id',
+        'parent_id',
+    ];
+
     public function commentable()
     {
         return $this->morphTo();
+    }
+
+    public function children()
+    {
+        return $this->hasMany(Comment::class,'parent_id');
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Comment::class,'parent_id');
     }
 }
